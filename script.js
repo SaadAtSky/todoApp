be3d516f06560f670a1abe649c9e21372bfd5621
Saad-322 <saady6nov@gmail.com>
2023-07-14 15:56:32 +0100

    intial template

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-let json_data = [
+var json_data = [
     {
-        title: "saad"
+      "userId": 1,
+      "id": 1,
+      "title": "delectus aut autem",
+      "completed": false
     },
     {
-        title: "ahmad"
+      "userId": 1,
+      "id": 2,
+      "title": "quis ut nam facilis et officia qui",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "title": "fugiat veniam minus",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 4,
+      "title": "et porro tempora",
+      "completed": true
+    },
+    {
+      "userId": 1,
+      "id": 5,
+      "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+      "completed": false
     }
-]
-let list = document.querySelector(".flex-container");
-json_data.forEach(item => {
-    let n = document.createElement("div");
-    n.setAttribute("class","flex-item")
-    n.innerText = item.title;
-    list.append(n);
-})
+  ];
