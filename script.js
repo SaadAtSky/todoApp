84443a1c214cdd817e1aacfcac1891b3be5f15db
Saad-322 <saady6nov@gmail.com>
2023-07-15 12:29:11 +0100

    local storage solu implemented for add and delete

--- script.js
+++ script.js
@@ -31,6 +31,25 @@
 //     }
 //   ];
 
+const storeLocal = (newTodo) => {
+    // get stuff from LC
+    let currentData = JSON.parse(localStorage.getItem("json_data"));
+    if(!currentData){// improvement: if null then make into array
+        currentData = []
+    }
+
+    // create new data
+    let newData = {
+        id: currentData.length,
+        title: newTodo,
+        completed: false
+    }
+
+    // push and store back
+    currentData.push(newData);
+    localStorage.setItem("json_data",JSON.stringify(currentData));  
+    return newData.id
+}
   const addDelete = (li) => {
     let del = document.createElement("a");
     del.setAttribute("class","btn btn-sm btn-danger m-1 delete");
@@ -40,19 +59,30 @@
 
   const del = (element) => {
     list.removeChild(element.parentElement)
+    // retrieve LC data
+    let data = JSON.parse(localStorage.getItem("json_data"));
+
+    // remove the specified ID
+    delete data[element.parentElement.dataset.id]
+
+    // return updated data back to LC
+    localStorage.setItem("json_data",JSON.stringify(data));
   }
 
   const addTodo = () => {
     let newTodo = document.querySelector("#newItem").value
-    addToList(newTodo)
+    let id = storeLocal(newTodo);
+    addToList(newTodo,id);
     addEventListener();
   }
 
-  const addToList = (item) => {
+  const addToList = (item,id) => {
     let li = document.createElement("li");
     li.append(document.createTextNode(item));
+    li.dataset.id = id;
     addDelete(li);
     list.append(li);
+    console.log(list)
   }
 
   const addEventListener = () => {
@@ -64,10 +94,16 @@
 
   let list = document.querySelector("#todo-list");
   document.querySelector(".btn").addEventListener("click",addTodo);
-//   json_data.forEach((item) => {
-//     addToList(item.title)
-//   })
-//   addEventListener();
+
+  let json_data = JSON.parse(localStorage.getItem("json_data"));
+  if(json_data){// improvement
+    json_data.forEach(item => {
+        if(item){
+            addToList(item.title);
+        }
+      })
+  }
+
 
 
 
